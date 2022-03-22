217235305fa563a8bae66de9c733eca31e0eeb93
Mohamedlee27 <leonk8070@gmail.com>
2022-03-22 23:10:36 +0300

    make the javascript functions all work

--- pizza.js
+++ pizza.js
@@ -51,3 +51,127 @@ $(document).ready(function(){
                 sizeSummary = sizes[i].price;
             }
         }
+
+        for (let i = 0; i < crusts.length; i++){
+            if(pizzaCrust === crusts[i].crust){
+                crustTotal += crusts[i].price;
+                crustSummary = crusts[i].price;
+            }
+        }
+
+      
+        for (let i = 0; i < toppings.length; i++){
+            if(pizzaTopping === toppings[i].topping){
+                toppingTotal += toppings[i].price;
+                toppingSummarry = toppings[i].price;
+            }
+        }
+
+        let total = sizeTotal + crustTotal + toppingTotal;
+        console.log(total);
+        $(".summary").show();
+        $(".order-button").hide();
+        $("#pizza-size").html($("#size option:selected").text() + " - " + sizeTotal);
+        $("#pizza-crusts").html($("#crusts option:selected").text() + " - " + crustTotal);
+        $("#pizza-topping").html($("#toppings option:selected").text() + " - " + toppingTotal);
+        $("#total").html(total);
+        
+    })
+    $("#order-add").click(function(event){
+       
+        orderNumber += 1;
+     
+        let pizzaSize = $("#size option:selected").val();
+        let pizzaCrust = $("#crusts option:selected").val();
+        let pizzaTopping = $("#toppings option:selected").val();
+
+       
+        let sizes = [small, medium, large];
+        let crusts = [stuffed, crispy, glutten];
+        let toppings = [pepporoni, mushroom, bacon];
+
+        let sizeSummary = "";
+        let crustSummary = "";
+        let toppingSummarry = "";
+
+
+        let sizeTotal = 0;
+        let crustTotal = 0;
+        let toppingTotal = 0;
+
+        for (let i = 0; i < sizes.length; i++){
+            if (pizzaSize === sizes[i].size){
+                sizeTotal += sizes[i].price;
+                sizeSummary = sizes[i].price;
+            }
+        }
+
+
+        for (let i = 0; i < crusts.length; i++){
+            if(pizzaCrust === crusts[i].crust){
+                crustTotal += crusts[i].price;
+                crustSummary = crusts[i].price;
+            }
+        }
+
+       
+        for (let i = 0; i < toppings.length; i++){
+            if(pizzaTopping === toppings[i].topping){
+                toppingTotal += toppings[i].price;
+                toppingSummarry = toppings[i].price;
+            }
+        }
+
+        let total = sizeTotal + crustTotal + toppingTotal;
+        console.log(total);
+        $(".summary").show();
+        $(".order-button").hide();
+        $("#pizza-size").html($("#size option:selected").text() + " - " + sizeTotal);
+        $("#pizza-crusts").html($("#crusts option:selected").text() + " - " + crustTotal);
+        $("#pizza-topping").html($("#toppings option:selected").text() + " - " + toppingTotal);
+        $("#total").html(total);
+
+        let tableRow = '<tr><th scope="row">' + orderNumber + '</th><td id="pizza-size">' + $("#size option:selected").text() + '---' + sizeSummary + '</td><td id="pizza-topping">' + $("#toppings option:selected").text() + '---' + toppingSummarry + '</td><td id="pizza-crusts">' + $("#crusts option:selected").text() + '---' + crustSummary + '</td><td id="total">' + total + '</td></tr>';
+        $(".output").append(tableRow);
+    })
+
+    $("#checkout").click(function(event){
+        let confirmation = confirm("Confirmed! Do you want your order to be delivered? Kenyan Shillings 250 will be charged.");
+        let pizzaSize = $("#size option:selected").val();
+        let pizzaCrust = $("#crusts option:selected").val();
+        let pizzaTopping = $("#toppings option:selected").val();
+
+        let sizes = [small, medium, large];
+        let crusts = [stuffed, crispy, glutten];
+        let toppings = [pepporoni, mushroom, bacon];
+
+        let sizeTotal = 0;
+        let crustTotal = 0;
+        let toppingTotal = 0;
+        for (let i = 0; i < sizes.length; i++){
+            if (pizzaSize === sizes[i].size){
+                sizeTotal += sizes[i].price;
+            }
+        }
+        for (let i = 0; i < crusts.length; i++){
+            if(pizzaCrust === crusts[i].crust){
+                crustTotal += crusts[i].price;
+            }
+        }
+        for (let i = 0; i < toppings.length; i++){
+            if(pizzaTopping === toppings[i].topping){
+                toppingTotal += toppings[i].price;
+            }
+        }
+
+        let total = sizeTotal + crustTotal + toppingTotal;
+        let grandTotal = total * orderNumber;
+        if (confirmation){
+            $("#grandTotal").html("Your Total is " + (grandTotal += 250) + " Thank you and enjoy");
+        }
+        else {
+            ("#grandTotal").html("Your Total is " + (grandTotal) + " Thank you and enjoy");
+        }
+        $(".add-another-order").hide();
+    })
+})
