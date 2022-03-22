e9a052bf0f99f338d6b5a7f9cfa8c75af0be9a93
Mohamedlee27 <leonk8070@gmail.com>
2022-03-22 22:33:21 +0300

    added piza functions

---
+++ pizza.js
@@ -0,0 +1,14 @@
+function PizzaSize(size, price){
+    this.size = size;
+    this.price = price;
+}
+
+function PizzaCrust(crust, price){
+    this.crust = crust;
+    this.price = price;
+}
+
+function PizzaTopping(topping, price){
+    this.topping = topping;
+    this.price = price;
+}
