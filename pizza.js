75e6e5cde38d6130c44a85de097fd652e08a2cf0
Mohamedlee27 <leonk8070@gmail.com>
2022-03-22 22:42:25 +0300

    made functions working

--- pizza.js
+++ pizza.js
@@ -12,6 +12,7 @@ function PizzaTopping(topping, price){
     this.topping = topping;
     this.price = price;
 }
+let orderNumber = 0;
 
 let small = new PizzaSize("Small", 300);
 let medium = new PizzaSize("Medium", 500);
@@ -20,3 +21,14 @@ let large = new PizzaSize("Large", 1000);
 let stuffed = new PizzaCrust("Stuffed", 150);
 let crispy = new PizzaCrust("Crispy", 300);
 let glutten = new PizzaCrust("Glutten-free", 120);
+
+let pepporoni = new PizzaTopping("Pepporoni", 50);
+let mushroom = new PizzaTopping("Mushroom", 100);
+let bacon = new PizzaTopping("Bacon", 150);
+
+$(document).ready(function(){
+    $("#order-button").click(function(event){
+        orderNumber += 1;
+        let pizzaSize = $("#size option:selected").val();
+        let pizzaCrust = $("#crusts option:selected").val();
+        let pizzaTopping = $("#toppings option:selected").val();
