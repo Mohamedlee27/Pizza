5c0f4ff3fc882317be7b41315b6719db6b206260
Mohamedlee27 <leonk8070@gmail.com>
2022-03-22 22:38:43 +0300

    added pricings for pizza crust and size

--- pizza.js
+++ pizza.js
@@ -12,3 +12,11 @@ function PizzaTopping(topping, price){
     this.topping = topping;
     this.price = price;
 }
+
+let small = new PizzaSize("Small", 300);
+let medium = new PizzaSize("Medium", 500);
+let large = new PizzaSize("Large", 1000);
+
+let stuffed = new PizzaCrust("Stuffed", 150);
+let crispy = new PizzaCrust("Crispy", 300);
+let glutten = new PizzaCrust("Glutten-free", 120);
